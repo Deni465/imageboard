538fb3af734da7992f8f1ff12c14e728009bad7a
Deni465 <denise.bamberg@web.de>
2022-10-12 19:19:08 +0200

    get back changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,22 @@
-import * as Vue from './vue.js';
+import * as Vue from "./vue.js";
+
+Vue.createApp({
+    data() {
+        return {
+            headline: "My Image Board",
+            cards: [],
+            headlineCssClass: "headline",
+        };
+    },
+
+    mounted() {
+        this.state = "mounted";
+
+        fetch("/cards")
+            .then((res) => res.json())
+            .then((cards) => {
+                this.cards = cards;
+                console.log(cards);
+            });
+    },
+}).mount("#main");
