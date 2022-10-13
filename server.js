1f3c7f1e686f9ddb3d59f95766f80afe316a0e11
Deni465 <denise.bamberg@web.de>
2022-10-13 17:43:15 +0200

    YEEEEAAAHH, did the aws thing

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 require("dotenv").config();
 const { PORT = 8080 } = process.env;
 const { uploader } = require("./middleware.js");
+const aws = require("aws-sdk");
+const fs = require("fs");
 
 const cards = [
     {
@@ -21,37 +23,19 @@ const cards = [
         title: "Why You Should Bring Your Dog To Work",
         text: "On Friday, offices around the country celebrated the 15th annual Take Your Dog to Work Day. Though the event's primary goal is to raise awareness for pet adoption, the unanticipated impact may be a slightly more relaxing work environment for any office choosing to participate.",
     },
-    {
-        url: "https://source.unsplash.com/75S9fpDJVdo/300x510",
-        title: "NASA Has Found Hundreds Of Potential New Planets",
-        text: "NASA released a list of 219 new “planet candidates discovered by the Kepler space telescope, 10 of which are similar to Earth’s size and may be habitable by other life forms.",
-    },
-    {
-        url: "https://source.unsplash.com/71u2fOofI-U/300x510",
-        title: "This Is Your Body And Brain On Coffee",
-        text: " Drinking more caffeine during the coronavirus lockdown? Here's how it can affect you over time and advice on making it better for you.",
-    },
-    {
-        url: "https://i.pinimg.com/564x/bf/59/f3/bf59f3370c32965d6cf0b829c3b3ec06.jpg",
-        title: "Why You Should Bring Your Dog To Work",
-        text: "On Friday, offices around the country celebrated the 15th annual Take Your Dog to Work Day. Though the event's primary goal is to raise awareness for pet adoption, the unanticipated impact may be a slightly more relaxing work environment for any office choosing to participate.",
-    },
-    {
-        url: "https://i.pinimg.com/564x/e3/4b/c2/e34bc2083adb6dfff539164e01ddad7f.jpg",
-        title: "NASA Has Found Hundreds Of Potential New Planets",
-        text: "NASA released a list of 219 new “planet candidates discovered by the Kepler space telescope, 10 of which are similar to Earth’s size and may be habitable by other life forms.",
-    },
-    {
-        url: "https://i.pinimg.com/564x/8c/1e/89/8c1e89c4e9f86dbeda851f6349302b96.jpg",
-        title: "This Is Your Body And Brain On Coffee",
-        text: " Drinking more caffeine during the coronavirus lockdown? Here's how it can affect you over time and advice on making it better for you.",
-    },
-    {
-        url: "https://i.pinimg.com/564x/ba/bf/3d/babf3d2945911e99ce9df163d12f66f0.jpg",
-        title: "Winter Season Is Coming",
-        text: "Everyone feels some magic in the air, because of frosts, crystal nights, snow and just this amazing atmosphere.",
-    },
 ];
+
+let secrets;
+if (process.env.NODE_ENV == "production") {
+    secrets = process.env; // in prod the secrets are environment variables
+} else {
+    secrets = require("./secrets.json"); // in dev they are in secrets.json which is listed in .gitignore
+}
+
+const s3 = new aws.S3({
+    accessKeyId: secrets.AWS_KEY,
+    secretAccessKey: secrets.AWS_SECRET,
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
@@ -63,15 +47,36 @@ app.get("/cards", (req, res) => {
 
 app.post("/cards", uploader.single("file"), (req, res) => {
     if (req.file) {
-        res.json({
-            success: true,
-            message: "Thank you!",
-            path: `/uploads/${req.file.filename}`,
-        });
+        const { filename, mimetype, size, path } = req.file;
+        const promise = s3
+            .putObject({
+                Bucket: "spicedling",
+                ACL: "public-read",
+                Key: filename,
+                Body: fs.createReadStream(path),
+                ContentType: mimetype,
+                ContentLength: size,
+            })
+            .promise();
+
+        promise
+            .then(() => {
+                console.log("success");
+                // it worked!!!
+                res.json({
+                    success: true,
+                    message: "Thank you!",
+                    path: `https://s3.amazonaws.com/spicedling/${filename}`,
+                });
+            })
+            .catch((err) => {
+                // uh oh
+                console.log(err);
+            });
     } else {
         res.json({
             success: false,
-            message: "Upload failed!",
+            message: "You didn't pick a file!",
         });
     }
 });
